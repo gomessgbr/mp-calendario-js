53c31385c3c7f2f395484318d517a3bc37b99256
Gabriel Gomes <gabrielgomes98@gmail.com>
2025-01-29 19:07:09 -0300

    refactor: using new components on calendar display

--- src/components/calendar-display.tsx
+++ src/components/calendar-display.tsx
@@ -1,7 +1,12 @@
-export function CalendarDisplay(){
-  return(
-    <div className="p-11 rounded-lg border-1 border-gray-600">
-        Teste
+import { CalendarHeader } from "./calendar-header";
+import { CalendarWeekDays } from "./calendar-week-days";
+
+export function CalendarDisplay() {
+  const date = new Date();
+  return (
+    <div className="rounded-lg p-2 border-1 border-gray-600 ">
+     <CalendarHeader currentMonth="janeiro 2024"/>
+     <CalendarWeekDays currentDate={date}/>
     </div>
-  )
+  );
 }
